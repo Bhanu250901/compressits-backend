da3e74c0bd92faccd01c766de64db9ae11a97d8c
Bhanu <gandhambhanu2509@gmail.com>
2026-06-15 15:13:15 +0530

    register check

--- src/main/java/com/compressit/backend/controller/AuthController.java
+++ src/main/java/com/compressit/backend/controller/AuthController.java
@@ -50,7 +50,7 @@ public class AuthController {
     public String registerUser(
             @RequestBody User user
     ) {
-        System.out.println("REGISTER API CALLED: " + user.getEmail());
+        userRepository.save(user);        System.out.println("REGISTER API CALLED: " + user.getEmail());
         // EMAIL EXISTS
         if (
                 userRepository
